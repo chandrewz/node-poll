24d1804b08691201d96b7eda8f671a5e263eda91
Andrew Chan <chandrew@utexas.edu>
2015-07-09 12:58:15 -0500

    db test

--- index.js
+++ index.js
@@ -4,18 +4,6 @@ var app = express();
 // db.insert('polls', {'question': 'Who is the greatest?'});
 var pg = require('pg');
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM polls', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-})
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -30,7 +18,21 @@ app.get('/', function(request, response) {
   	// response.send(e)
   // });
   // response.send(db)
+
+  response.send('OK');
 });
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM polls', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));

e27996f34831532842ae4caca9820e7f02cd46cc
Andrew Chan <chandrew@utexas.edu>
2015-07-11 19:37:12 -0500

    PollController

---
+++ controllers/PollController.js
@@ -0,0 +1,13 @@
+/**
+ * Models
+ */
+var models = require('./models/models');
+var knex = models.knex;
+var Poll = models.Poll;
+var PollOption = models.PollOption;
+
+exports.getPoll = function(request, response) {
+	Poll.where({ id: request.params.id }).fetch({ withRelated: ['options'] }).then(function(poll) {
+		response.send(poll.toJSON());
+	});
+}

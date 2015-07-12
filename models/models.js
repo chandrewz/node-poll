8b1e00ea7c882da283b4be21fc2953b833a27483
Andrew Chan <chandrew@utexas.edu>
2015-07-11 20:26:58 -0500

    redo PollOption model?

--- models/models.js
+++ models/models.js
@@ -25,7 +25,7 @@ var Poll = bookshelf.Model.extend({
 var PollOption = bookshelf.Model.extend({
 	tableName: 'options',
 	poll: function() {
-		return this.hasOne(Poll, 'id');
+		return this.belongsTo(Poll);
 	}
 });
 

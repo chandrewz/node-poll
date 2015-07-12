a8a06d0a36ea9ac61a843c4986274852b9b1a68d
Andrew Chan <chandrew@utexas.edu>
2015-07-12 17:36:05 -0500

    auto-add options and success modal

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,40 @@
+
 $( "form" ).submit(function( event ) {
 	event.preventDefault();
 	var formArray = $( this ).serializeArray();
+
 	var postData = {};
 	postData.name = formArray[0].value
 	postData.options = [];
 	for (var i = 1; i < formArray.length; i++) {
-		postData.options.push(formArray[i].value);
+		if (formArray[i].length !== 0 && formArray[i].value.trim()) {
+			postData.options.push(formArray[i].value);
+		}
 	}
+	postData.track_ip = $('#track').is(':checked');
 
 	$.ajax({
 		type: "POST",
 		url: "api/poll",
 		data: JSON.stringify(postData),
-		success: function(){ alert('Poll created'); },
+		success: function(data) {
+			$('#modal-title').text('Wow, poll created!');
+			$('#modal-body').html('You can see your poll here: <a href="http://node-poll.herokuapp.com/poll/' + data.id + '">http://node-poll.herokuapp.com/poll/' + data.id + '</a>');
+		},
 		dataType: "json",
 		contentType : "application/json"
 	});
 });
+
+/**
+ * If the last option is filled, add another option.
+ */
+function lastOption() {
+	$('.last-option').on('input', function() {
+		$('#options').append('<div class="form-group"><input name="option" type="text" class="form-control floating-label last-option" placeholder="Enter poll option"></div>')
+		$(this).removeClass('last-option');
+		$(this).unbind();
+		lastOption();
+	});
+}
+lastOption();
